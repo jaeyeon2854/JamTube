197f2404219a332261e8bbce59897517dd4f817d
jaeyeon2854 <wodus2854@naver.com>
2023-11-26 20:14:30 +0900

    req,res 정리

--- src/server.js
+++ src/server.js
@@ -3,6 +3,16 @@ import express from "express";
 const POST = 4000;
 
 const app = express();
+ 
+const handelHome = (req,res) => {
+   return res.send("<h1>I sitll love you.</h1>")
+}
+
+const handleLogin = (req,res) =>{
+    return res.send({message:"Login here."})
+}
+app.get("/",handelHome);
+app.get("/login",handleLogin);
 
 const handleListening = () => 
     console.log(`Server listening on port http://localhost:${POST} 🚀`);
